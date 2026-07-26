73954f5fb13fa88c8c953aa59e940ba33346ebda
Raja kumar <aryankumar29sep@gmail.com>
2026-07-26 23:26:58 +0530

    Time: 3 ms (65.12%) | Memory: 31.6 MB (46.18%) - LeetSync

---
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+        int firstMax = INT_MIN;
+        int secondMax = INT_MIN;
+        int thirdMax = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            // if (firstMin >= nums[i]) {
+            //     secondMin = firstMin;
+            //     firstMin = nums[i];
+            // } else if (secondMin > nums[i]) {
+            //     secondMin = nums[i];
+            // }
+
+            // if (firstMax <= nums[i]) {
+            //     thirdMax = secondMax;
+            //     secondMax = firstMax;
+            //     firstMax = nums[i];
+            // } else if (secondMax <= nums[i]) {
+            //     thirdMax = secondMax;
+            //     secondMax = nums[i];
+            // } else if (thirdMax < nums[i]) {
+            //     thirdMax = nums[i];
+            // }
+
+            // Two minimums
+            if (nums[i] <= firstMin) {
+                secondMin = firstMin;
+                firstMin = nums[i];
+            } else if (nums[i] < secondMin) {
+                secondMin = nums[i];
+            }
+
+            // Three maximums
+            if (nums[i] >= firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = nums[i];
+            } else if (nums[i] >= secondMax) {
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            } else if (nums[i] > thirdMax) {
+                thirdMax = nums[i];
+            }
+        }
+        return max(1LL * firstMax * secondMax * thirdMax,
+                   1LL * firstMin * secondMin * firstMax);
+    }
+};
