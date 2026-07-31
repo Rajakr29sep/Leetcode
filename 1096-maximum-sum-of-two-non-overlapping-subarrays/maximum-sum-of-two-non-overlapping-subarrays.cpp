af7a82925689570730e80ae2f6b39e083503c36b
Raja kumar <aryankumar29sep@gmail.com>
2026-07-31 12:21:04 +0530

    Time: 6 ms (17.21%) | Memory: 14.7 MB (5.01%) - LeetSync

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    int solve(vector<int>& prefixSum, int len1, int len2, int s, int e) {
+    int solve(vector<int>& prefixSum, int len1, int len2, int i, int count,
+              vector<vector<int>>& dp) {
+
+        if (i >= prefixSum.size() || count >= 2) {
+            return 0;
+        }
+        if (dp[i][count] != -1)
+            return dp[i][count];
         int n = prefixSum.size();
-        int maxi = INT_MIN;
-        for (int i = 0; i <= n - len2; i++) {
-            if (i + len2 - 1 < s || i > e) {
-                int sum1 = (prefixSum[i + len2 - 1] -
-                            (i - 1 >= 0 ? prefixSum[i - 1] : 0));
-                maxi = max(sum1, maxi);
+        int maxi = 0;
+        if (count == 0) {
+            if (i + len1 - 1 < prefixSum.size()) {
+                maxi = max(
+                    prefixSum[i + len1 - 1] - (i > 0 ? prefixSum[i - 1] : 0) +
+                        solve(prefixSum, len1, len2, i + len1, count + 1, dp),
+                    solve(prefixSum, len1, len2, i + 1, count, dp));
+            } else {
+                maxi = solve(prefixSum, len1, len2, i + 1, count, dp);
+            }
+
+        } else if (count == 1) {
+            if (i + len2 <= prefixSum.size()) {
+                maxi = max(
+                    prefixSum[i + len2 - 1] - (i > 0 ? prefixSum[i - 1] : 0) +
+                        solve(prefixSum, len1, len2, i + len2, count + 1, dp),
+                    solve(prefixSum, len1, len2, i + 1, count, dp));
+            } else {
+                maxi = solve(prefixSum, len1, len2, i + 1, count, dp);
             }
         }
-        return maxi;
+
+        return dp[i][count] = maxi;
     }
 
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
@@ -20,14 +41,9 @@ public:
         for (int i = 1; i < n; i++) {
             prefixSum[i] = nums[i] + prefixSum[i - 1];
         }
-        int maxi = INT_MIN;
-        for (int i = 0; i <= n - firstLen; i++) {
-            int sum1 = (prefixSum[i + firstLen - 1] -
-                        (i - 1 >= 0 ? prefixSum[i - 1] : 0));
-            maxi = max(sum1 + solve(prefixSum, firstLen, secondLen, i,
-                                    i + firstLen - 1),
-                       maxi);
-        }
-        return maxi;
+        vector<vector<int>> dp1(n + 1, vector<int>(2, -1));
+        vector<vector<int>> dp2(n + 1, vector<int>(2, -1));
+        return max(solve(prefixSum, firstLen, secondLen, 0, 0, dp1),
+                   solve(prefixSum, secondLen, firstLen, 0, 0, dp2));
     }
 };
